7f905da21efa1a50be8e698b69fb4baa1d6464b9
Calvin Houng <siegyz@hotmail.com>
2012-09-24 11:49:42 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// console.log("main.js is loaded");
+// $("img").width(200);
+
+$(document).ready(function(){
+
+	$("img").on("dblclick", function(){
+// here.
+	$("img").width(200);
+
+	$("h1").fadeOut();
+
+	$(".booboo").slideUp();
+
+	$("#rahul".hide();
+	});
+
+//$("img").click(function(){
+	// here.
+//	$("img").width(200);
+//	});
+});
